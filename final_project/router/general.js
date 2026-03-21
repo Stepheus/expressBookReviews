fba4cda66d9d52b25b574becac8e5625fa74f5ec
Stepheus <stephen.iboudah@outlook.com>
2026-03-21 00:28:03 -0400

    global route completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,11 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function formatBook(data){
+    data = JSON.stringify(data);
+    data = JSON.parse(data);
+    return data;
+}
 
 public_users.post("/register", (req,res) => {
   //Register as user 
@@ -13,31 +18,45 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-return res.status(300).send(JSON.stringify({books},null, 4));
+return res.status(200).send(JSON.stringify({books},null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let isbnBook = books[isbn];
+  return res.status(200).send(JSON.stringify(isbnBook));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const name = req.params.author;
+
+    //formating the data for manipulation
+    books = formatBook(books);
+
+    //accessing value needed
+   let filteredBook = Object.fromEntries(Object.entries(books).filter(([key, value])=> value.author.toLowerCase() === name.toLowerCase()));
+
+   
+    return res.status(200).send(JSON.stringify(filteredBook, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const name = req.params.title;
+    books = formatBook(books);
+    let filteredBook = Object.fromEntries(Object.entries(books).filter(([key, value])=> value.title.toLowerCase() === name.toLowerCase()));
+    return res.status(200).send(JSON.stringify(filteredBook, null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  books = formatBook(books);
+  let filteredReview = books[isbn].reviews;
+  return res.status(200).send(JSON.stringify(filteredReview, null, 4));
 });
 
 module.exports.general = public_users;
